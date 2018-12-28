59c13cea984db9377c806b5a4bcbc77979383841
URAYAMA,kazuaki <kazuaki.urayama@gmail.com>
2018-12-28 16:03:34 +0900

    update TC2

--- Scripts/TC2/Script1545945804432.groovy
+++ Scripts/TC2/Script1545945804432.groovy
@@ -1,3 +1,14 @@
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 
-WebUI.comment("Hello from TC2")
+// ExpandoMetaClass http://groovy-lang.org/metaprogramming.html#metaprogramming_emc
+
+// https://github.com/katalon-studio/katalon-studio-testing-framework/blob/master/Include/scripts/groovy/com/kms/katalon/core/keyword/builtin/CommentKeyword.groovy
+com.kms.katalon.core.keyword.builtin.CommentKeyword.metaClass.comment = { 
+	String message -> 
+		println message;
+		logger.logInfo(message)
+	}
+
+String tcName = "TC2"
+
+WebUI.comment("Hello from " + tcName)
